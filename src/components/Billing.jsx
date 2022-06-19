2c94c454a54ba429f7df278c07c765b62d523db0
Sahand Masoleh <sahand.masoleh@gmail.com>
2022-06-19 23:47:20 +0200

    desktop view

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -3,11 +3,13 @@ import Toggle from "./Toggle";
 
 function Billing({ handleDiscount, hasDiscount }) {
 	return (
-		<div className="selecton__billing billing">
+		<div className="selection__billing billing">
 			<span className="billing__period">Monthly Billing</span>
 			<Toggle handleDiscount={handleDiscount} hasDiscount={hasDiscount} />
 			<span className="billing__period">Yearly Billing</span>
-			<span className="billing__discount">-25%</span>
+			<span className="billing__discount">
+				-25% <span className="billing__discount-text">discount</span>
+			</span>
 		</div>
 	);
 }
